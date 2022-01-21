89c217fdd9dc221cce6bf2e2500fadb24b1af24d
ericatkins8 <eric.atkins@hilliardschools.org>
2022-01-21 10:49:18 -0500

    a

--- project/Main.gd
+++ project/Main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _process(delta):
-	$Camera2D.set_position($KinematicBody2D.get_position())
+	$Camera2D.set_position($Player.get_position())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
