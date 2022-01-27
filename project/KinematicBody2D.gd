0cad8c513470d1f28b771f601c931fb433994967
ericatkins8 <94485199+ericatkins8@users.noreply.github.com>
2022-01-27 10:55:39 -0500

    s

--- project/KinematicBody2D.gd
+++ project/KinematicBody2D.gd
@@ -1,21 +1,26 @@
 extends KinematicBody2D
 
 var speed = 200  # speed in pixels/sec
-var velocity = Vector2.ZERO
+var velocity = Vector2(0,0)
+var friction = .35
+var maxspeed = 300
+var acceleration = maxspeed * (1 - friction)
+
 
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
+		velocity.x += acceleration
 	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
+		velocity.x -= acceleration
 	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
+		velocity.y += acceleration
 	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	# Make sure diagonal movement isn't faster
-	velocity = velocity.normalized() * speed
+		velocity.y -= acceleration
+	
+	velocity = velocity.normalized() * acceleration
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+
