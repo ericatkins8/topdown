03e4112541a88a4ba776d612ec09f75b58b89f02
ericatkins8 <94485199+ericatkins8@users.noreply.github.com>
2022-02-15 20:46:53 -0500

    animations

--- project/KinematicBody2D.gd
+++ project/KinematicBody2D.gd
@@ -1,9 +1,8 @@
 extends KinematicBody2D
 
-var speed = 200  # speed in pixels/sec
 var velocity = Vector2(0,0)
-var friction = .35
-var maxspeed = 300
+var friction = .3
+var maxspeed = 150
 var acceleration = maxspeed * (1 - friction)
 
 
@@ -11,13 +10,21 @@ func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += acceleration
+		$CollisionShape2D/AnimatedSprite.play("sidewalk")
+		$CollisionShape2D/AnimatedSprite.flip_h = false
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= acceleration
+		$CollisionShape2D/AnimatedSprite.play("sidewalk")
+		$CollisionShape2D/AnimatedSprite.flip_h = true
 	if Input.is_action_pressed('ui_down'):
+		$CollisionShape2D/AnimatedSprite.play("walk")
 		velocity.y += acceleration
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= acceleration
-	
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed('ui_left') or Input.is_action_pressed('ui_down') or Input.is_action_pressed('ui_up'):
+		$CollisionShape2D/AnimatedSprite.play()
+	else:
+		$CollisionShape2D/AnimatedSprite.stop()
 	velocity = velocity.normalized() * acceleration
 
 func _physics_process(_delta):
